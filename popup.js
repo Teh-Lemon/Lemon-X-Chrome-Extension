57c0e2743ffae989f50ece7831afb5a5066e0199
Teh-Lemon <Anthonyllee99@gmail.com>
2014-02-24 01:57:53 +0000

    MAL Search improvement
    New
    - MAL search text box now focuses after a change has been made to the
    search type

--- popup.js
+++ popup.js
@@ -6,8 +6,8 @@ function main()
 
 // Event handler for MAL submit button
 // Search MAL based on search terms and drop down menu
-function clickHandlerMALSubmitButton(e) {
-
+function clickHandlerMALSubmitButton(e) 
+{
 	// Text box values
 	var searchTerms = document.getElementById('MALTextBox').value;
 	// Used to direct link to correct search url
@@ -46,10 +46,18 @@ function clickHandlerMALSubmitButton(e) {
 	}
 }
 
+// Event handler for changing the drop down menu item
+// Focuses on the text box when changed
+function changeHandlerMALTypeMenu(e)
+{
+	document.getElementById('MALTextBox').focus();
+}
+
 // Add event listeners once the DOM has fully loaded by listening for the
 // `DOMContentLoaded` event on the document, and adding your listeners to
 // specific elements when it triggers.
 document.addEventListener('DOMContentLoaded', function () {
 	document.getElementById('MALSubmitButton').addEventListener('click', clickHandlerMALSubmitButton);
+	document.getElementById('MALType').addEventListener('change', changeHandlerMALTypeMenu);
 	main();
 });
